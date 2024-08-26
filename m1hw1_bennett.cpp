91750727bce8c8c390365419d3aa355b12cb7962
bennettg3362 <bennettg3362@student.faytechcc.edu>
2024-08-26 11:29:20 -0400

    Add files via upload

---
+++ m1hw1_bennett.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  string filmName = "Mishima: A Life in Four Chapters";
+  string director = "Paul Schrader";
+  int year = 1985;
+  double gross = 569.9;
+
+  cout << "The film " << filmName << ", directed by " << director;
+  cout << ", came out in " << year << ", and had a worldwide gross of about ";
+  cout << gross << " thousand dollars." << endl;
+
+  cout << filmName << " is a biopic about Japanese author Yukio Mishima." << endl;
+  cout << "The first three segments of the film parallel events in Mishima's life with his novels, while the fourth culminates in his attempted coup of the Japanese government and subsequent suicide." << endl;
+  cout << "Here are some quotes from the film: " << endl;
+  cout << "\"When a man reaches forty, he has no chance to die beautifully. No matter how he tries, he will die of decay. He must compel himself to live\"" << endl;
+  cout << "\"The instant the blade tore open his flesh, the bright disk of the sun soared up behind his eyelids and exploded lighting the sky for an instant.\"" << endl;
+  
+}
---
+++ m1lab1_bennett.cpp
@@ -0,0 +1,48 @@
+// CSC 134
+// M1LAB - Apple Orchard
+// Gabrel Bennett
+// 08-21-2024
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+  // Set up variables
+  string name;
+  string last_name;
+  int num_apples;
+  double price_per_apple;
+  double total_cost;
+
+  // Get user input 
+  cout << "What is your first name? " << endl;
+  cin >> name;
+  cout << "What is your last name? " << endl;
+  cin >> last_name;
+  cout << "How many apples in stock? " << endl;
+  cin >> num_apples;
+  cout << "How much is one apple? " << endl;
+  cin >> price_per_apple;
+
+  // Say hello
+  cout << "Welcome to " << name << " " << last_name;
+  cout << "\'s apple orchard!" << endl;
+  cout << "We have " << num_apples << " in stock." << endl;
+  cout << "The cost of one apple is $";
+  cout << price_per_apple << "." << endl; 
+
+  // Ask some questions
+  int your_apples = 0;
+  cout << "How many apples would you like? " << endl;
+  cin >> your_apples;
+  
+
+  // Give the answers
+  if (your_apples > num_apples) {
+    cout << "We don't have that many apples!" << endl;
+  } else {
+    cout << "You have " << your_apples << " apples" << endl;
+    total_cost = your_apples * price_per_apple;
+    cout << "Your total price is $" << total_cost << endl;
+  }
+}
