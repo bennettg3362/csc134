2fdbe02db9540960d299b6a3d27edbb2de83ee18
bennettg3362 <bennettg3362@student.faytechcc.edu>
2024-11-11 16:02:15 +0000

    made m6t1

---
+++ m6t1_bennett.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// CSC 134
+// M6T1
+// bennettg
+// 11/11/2024
+
+//global constant
+const int NUM_DAYS = 5;
+
+double getAverage(int total, int members);
+
+int main() {
+    string days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    int carsEachDay[NUM_DAYS];
+    int total = 0;
+    int input;
+    double average;
+
+    for (int i = 0; i < NUM_DAYS; i++) {
+        cout << "Please enter the count for " << days[i] << ": ";
+        cin >> input;
+        carsEachDay[i] = input;
+        total += input;
+    }
+    cout << endl;
+
+    average = total / (NUM_DAYS * 1.0);
+
+    for (int i = 0; i < NUM_DAYS; i++) {
+        cout << days[i].substr(0, 2) << ": ";
+        for (int j = 0; j < carsEachDay[i]; j++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+    cout << "Total: " << total << endl;
+    cout << "Average: " << average << endl;
+}
